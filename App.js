aa4dcab0412fceec193fc32d7dfda3ae77cc19e1
Lailson Henrique <lailson.pi@gmail.com>
2021-07-30 16:38:35 -0300

    criando estrutura da busca

--- App.js
+++ App.js
@@ -1,11 +1,45 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Text, View } from 'react-native';
+import styled from 'styled-components/native';
+
+const Tela = styled.View`
+  flex: 1;
+`
+
+const Cabecalho = styled.View`
+  background-color: #1827AC;
+  height: 65px;
+  padding: 0 30px;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`
+
+const Busca = styled.TextInput`
+  color: #fff;
+  font-size: 20px;
+`
+
+const Botao = styled.TouchableOpacity``
+
+const BuscaImagem = styled.Image`
+  width: 30px;
+  height: 30px;
+`
+
 
 export default function App() {
+  const [nome, alteraNome] = useState('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Tela >
+      <Cabecalho>
+        <Busca placeholder="Digite o nome do filme..." value={nome} onChangeText={ (filme) => {alteraNome}} placeholderTextColor="#cecece"/>
+        <Botao>
+          <BuscaImagem source={require('./assets/icons8-pesquisar-50.png')}/>
+        </Botao>
+      </Cabecalho>
+      
+    </Tela>
   );
 }
